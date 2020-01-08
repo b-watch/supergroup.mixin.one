061369a59763a805ad1f6f785af52f7b62706a6f
Wei <5h3ll3x@gmail.com>
2020-01-08 22:21:09 +0900

    add new scope for message represent permission

--- web/src/App.vue
+++ web/src/App.vue
@@ -23,7 +23,7 @@ export default {
   },
   methods: {
     redirectToOAuth(payload) {
-      let url = `https://mixin.one/oauth/authorize?client_id=${CLIENT_ID}&scope=PROFILE:READ+ASSETS:READ&response_type=code&return_to=${encodeURIComponent(
+      let url = `https://mixin.one/oauth/authorize?client_id=${CLIENT_ID}&scope=PROFILE:READ+ASSETS:READ+MESSAGES:REPRESENT&response_type=code&return_to=${encodeURIComponent(
         OAUTH_CALLBACK_URL
       )}`;
       window.location.href = url;
