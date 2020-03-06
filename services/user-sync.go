f1bfc675503cb896614278d7354980cf87be6f77
yiplee <guoyinl@gmail.com>
2020-03-06 21:13:50 +0800

    add log

--- services/user-sync.go
+++ services/user-sync.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"encoding/json"
+	"log"
 
 	"github.com/MixinNetwork/supergroup.mixin.one/broker"
 	"github.com/MixinNetwork/supergroup.mixin.one/durable"
@@ -14,9 +15,11 @@ import (
 func SyncUsers(db *durable.Database) {
 	ctx := session.WithDatabase(context.Background(), db)
 	_ = broker.Sub(ctx, broker.Connect(), func(e *broker.Event) error {
+		log.Println("sync-user", "got event", e.Topic)
 		switch plugin.EventType(e.Topic) {
 		case plugin.EventTypeUserCreated, plugin.EventTypeUserPaid:
 			user := decodeUserFromEvent(e)
+			log.Println("sync-user", user.FullName, user.UserId)
 			return models.SyncUser(ctx, user)
 		default:
 			return nil
